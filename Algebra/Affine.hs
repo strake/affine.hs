93a72cd44de152b4c7e4eb97d3bbc69198274dfe
M Farkas-Dyck <strake888@gmail.com>
2018-05-16 19:44:58 -0800

    operators

--- Algebra/Affine.hs
+++ Algebra/Affine.hs
@@ -6,3 +6,12 @@ class Group (Diff a) => Affine a where
     type Diff a
     diff :: a -> a -> Diff a
     offset :: Diff a -> a -> a
+
+infix 6 .-.
+(.-.) :: Affine a => a -> a -> Diff a
+(.-.) = diff
+
+infixl 6 .+, .-
+(.+), (.-) :: Affine a => a -> Diff a -> a
+a .+ δ = offset δ a
+a .- δ = offset (invert δ) a
